e29d548c47ed029ce83aebd49bdd063fbcfe20a9
m2bass <121668589+m2bass@users.noreply.github.com>
2023-10-12 08:49:02 -0300

    Update w0492257_rock_paper_scissors_class_activity.py

--- w0492257_rock_paper_scissors_class_activity.py
+++ w0492257_rock_paper_scissors_class_activity.py
@@ -2,6 +2,11 @@
 
 # import modules here
 import random
+# Prints the rules of the game. Performs string concatenation
+print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
+      + "Rock vs Paper -> Paper wins \n"
+      + "Rock vs Scissors -> Rock wins \n"
+      + "Paper vs Scissors -> Scissor wins \n")
 
 #Global variable
 invalid_attempts = 3
